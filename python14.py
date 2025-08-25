d602c48d54050de219910e49bea30de497441a20
Harsha Vardhan Koduri <108460037+harshavardhankoduri@users.noreply.github.com>
2025-08-25 14:33:47 +0530

    Add files via upload

---
+++ python14.py
@@ -0,0 +1,14 @@
+n=5
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        if i==1:
+            print("*",end="")
+        elif i==n:
+            print("*",end="")
+        elif j==1:
+            print("*",end="")
+        elif j==n:
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
